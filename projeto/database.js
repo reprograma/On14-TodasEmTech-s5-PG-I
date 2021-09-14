a3e6f7f160fdb56cecb1aec8235552b6e9a413fb
Rafaela Dartora <siglyane@gmail.com>
2021-09-14 12:58:48 -0300

    Altera livros cadastrados

--- projeto/database.js
+++ projeto/database.js
@@ -1,59 +1,109 @@
+
+
 //cria o array de objetos estudantes 
 const livros = [
     {
-      nome: 'Insônia',
-      categoria: 'Fantasia',
+      nome: 'INSÔNIA',
+      autor: 'STEPHEN KING',
+      categoria: 'TERROR',
       paginas: 706,
-      recomenda: true,
+      recomenda: false,
       leu: false
     },
     {
-      nome: 'Drácula',
-      categoria: 'Terror',
+      nome: 'DRÁCULA',
+      autor: 'BRAM STROKER',
+      categoria: 'TERROR',
       paginas: 543,
       recomenda: false,
-      leu: true
+      leu: false
     },
     {
-      nome: 'Maus',
-      categoria: 'string',
+      nome: 'MAUS',
+      autor: 'ART SPIEGELMAN',
+      categoria: 'AUTOBIOGRAFIA',
       paginas: 296,
       recomenda: true,
       leu: true
     },
     {
-      nome: 'Laranja Mecânica',
-      categoria: 'string',
-      paginas: 352,
-      recomenda: false,
+      nome: 'LARANJA MECÂNICA',
+      autor: 'ANTHONY BURGESS',
+      categoria: 'CRIME',
+      paginas: 240,
+      recomenda: true,
+      leu: true
+    },
+    {
+      nome: 'PRATICAMENTE INOFENSIVA',
+      autor: 'DOUGLAS ADAM',
+      categoria: 'FICÇÃO CIENTÍFICA',
+      paginas: 223,
+      recomenda: true,
+      leu: true
+    },
+    {
+      nome: 'AS FONTES DO PARAÍSO',
+      autor: 'ARTHUR C. CLARKE',
+      categoria: 'FICÇÃO FIENTÍFICA',
+      paginas: 350,
+      recomenda: true,
+      leu: true
+    },
+    {
+      nome: '2001: UMA ODISSÉIA NO ESPAÇO',
+      autor: 'ARTHUR C. CLARKE',
+      categoria: 'FICÇÃO FIENTÍFICA',
+      paginas: 336,
+      recomenda: true,
       leu: false
     },
     {
-      nome: 'Praticamente Inofensiva',
-      categoria: 'Ficção',
-      paginas: 999,
+      nome: 'FUNDAÇÃO',
+      autor: 'ISAAC ASIMOV',
+      categoria: 'FICÇÃO FIENTÍFICA',
+      paginas: 307,
+      recomenda: true,
+      leu: false
+    },
+    {
+      nome: 'O UNIVERSO NUMA CASCA DE NOZ',
+      autor: 'STEPHEN HAWKING',
+      categoria: 'FÍSICA TEÓRICA',
+      paginas: 216,
       recomenda: true,
       leu: true
     },
     {
-      nome: 'string',
-      categoria: 'string',
-      paginas: 999,
+      nome: 'OS BOTÕES DE NAPOLEÃO: AS 17 MOLÉCULAS QUE MUDARAM A HISTÓRIA',
+      autor: 'PENNY LE COUTEUR',
+      categoria: 'CIÊNCIAS EXATASQUÍMICA',
+      paginas: 442,
       recomenda: true,
       leu: true
     },
     {
-      nome: 'string',
-      categoria: 'string',
-      paginas: 999,
+      nome: 'NIMONA',
+      autor: 'NOELLE STEVENSON',
+      categoria: 'QUADRINHOS',
+      paginas: 273,
       recomenda: true,
       leu: true
     },
     {
-      nome: 'string',
-      categoria: 'string',
-      paginas: 999,
-      recomenda: true,
+      nome: 'O PSICOPATA AMERICANO',
+      autor: 'BRET EASTON ELLIS',
+      categoria: 'HORROR',
+      paginas: 432,
+      recomenda: false,
+      leu: true
+    },
+    {
+      nome: 'SENHOR DOS ANÉIS: A SOCIEDADE DO ANEL',
+      autor: 'J. R. R. TOLKIEN',
+      categoria: 'LITERATURA FANTÁSTICA',
+      paginas: 273,
+      recomenda: false,
       leu: true
     },
 ]
