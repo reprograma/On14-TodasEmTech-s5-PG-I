265cae8afb32d2c83144cf8aaea6207f2fc4e634
ester <estersilvade@gmail.com>
2021-09-17 20:00:06 -0300

    subindo arquivo

--- projeto/database.js
+++ projeto/database.js
@@ -1,28 +1,154 @@
-module.exports = {
-  produtos: [
+const music  = [
+  
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Exagerado',
+      ano: 1985,
+      nomedoAlbum : 'Exagerado',
+      estilo : 'ROCK',
+      favorito :false
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Codinome Beija-flor',
+      ano: 1985,
+      nomedoAlbum : 'Exagerado',
+      estilo : 'ROCK',
+      favorito :true
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Ideologia',
+      ano: 1988,
+      nomedoAlbum : 'Ideologia',
+      estilo : 'ROCK',
+      favorito :true
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Brasil',
+      ano: 1988,
+      nomedoAlbum : 'Ideologia',
+      estilo : 'ROCK',
+      favorito :false
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Preciso Dizer que Te amo',
+      ano: 1987,
+      nomedoAlbum : 'Preciso Dizer que Te amo',
+      estilo : 'ROCK',
+      favorito :false
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'O Tempo Não Para ',
+      ano: 1988,
+      nomedoAlbum : 'Ideologia',
+      estilo : 'ROCK',
+      favorito :true
+      
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'Faz Parte do Meu Show',
+      ano: 1988,
+      nomedoAlbum : 'Ideologia',
+      estilo : 'ROCK',
+      favorito :false
     },
     {
-      nome: 'incluir'
+      cantor : 'Cazuza',
+      nomedaMusica: 'O Tempo Não Para',
+      ano: 1989,
+      nomedoAlbum : 'Ideologia',
+      estilo : 'ROCK',
+      favorito :true
     },
-  ],
-}
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Garganta ',
+      ano: 1999,
+      nomedoAlbum : 'Ana Carolina',
+      estilo : 'MPB',
+      favorito :true
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Quem De Nós Dois ',
+      ano: 2001,
+      nomedoAlbum : 'Ana Rita Joana Iracema e Carolina',
+      estilo : 'MPB',
+      favorito :true
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Uma Louca Tempestade ',
+      ano:2003 ,
+      nomedoAlbum : 'Estampado',
+      estilo : 'MPB',
+      favorito :true
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Rosas ',
+      ano: 2006,
+      nomedoAlbum : 'Dois Quartos',
+      estilo : 'MPB',
+      favorito :true
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'O Melhor Mim',
+      ano: 1999,
+      nomedoAlbum : 'Ana Carolina ',
+      estilo : 'MPB',
+      favorito :false
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Não Tem No Mapa',
+      ano:2019 ,
+      nomedoAlbum : 'Fogueira em Alto Mar',
+      estilo : 'MPB',
+      favorito :false
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'É isso aí',
+      ano:2005 ,
+      nomedoAlbum : 'Ana & Jorge',
+      estilo : 'MPB',
+      favorito :true
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Confesso',
+      ano: 2001 ,
+      nomedoAlbum : 'ANA RITA JOANA IRACEMA E CAROLINA',
+      estilo : 'MPB',
+      favorito :true
+
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Mais Que a Mim',
+      ano:2014 ,
+      nomedoAlbum : 'Ana Car9lina + Um ',
+      estilo : 'MPB',
+      favorito :false 
+    },
+    {
+      cantor : 'Ana Carolina ',
+      nomedaMusica: 'Problema',
+      ano: 2011,
+      nomedoAlbum : ' Ensaio De Cores',
+      estilo : 'MPB',
+      favorito :false
+    },
+    
+    
+    
+]
+
+module.exports = music
--- projeto/index.js
+++ projeto/index.js
@@ -1,8 +1,38 @@
-console.log('--------------------------------------')
-console.log('     Projeto            ')
-console.log('             Tassila Bomfim         ')
-console.log('--------------------------------------')
+const music = require('./database')
+console.table(music);
 
+music.sort((a, b) => a.ano - b.ano)
+console.table(music)
+const pegarObjeto = require('readline-sync')
+ const verEstilo = pegarObjeto.question('Você Deseja visualizar musica por estilo? (S/N) ').toLocaleUpperCase()
+if(verEstilo == 'S'){
+  console.log('Essaas são as musicas por estilo:')
+  console.log('Temos Rock e MPB')
+  const qualEstilo = pegarObjeto.question ('Qual é o estilo?').toLocaleUpperCase()
+  function estilo(music) {
+    return music.estilo === qualEstilo
+  }
+   const estilosencontrado = music.filter(estilo)
+   console.table(estilosencontrado)
+
+} else {
+  console.log( 'Okay ')
+  console.table( music)
+  
+}
+const verFvorito = pegarObjeto.question('Você Deseja visualizar musicas Favoritas ? (S/N) ').toLocaleUpperCase()
+if(verFvorito == 'S'){
+  console.log('Aqui estão algumas das minhas musicas favoritas ')
+  function favoritos (music){
+    return music.favorito === true
+  }
+   
+  console.table(music.filter(favoritos))
+
+
+}else{
+   console.log('Agradeço a sua curiosidade!')
+}
 
 //vamos instanciar, o nosso banco de dados
 
