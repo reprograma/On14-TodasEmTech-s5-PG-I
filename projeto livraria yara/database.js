0c23caccc2d08028deb84945939742768c024cbd
yara <yaralviana@gmail.com>
2021-09-18 05:07:52 -0300

    Projeto livraria - Será que agora vai?

---
+++ projeto livraria yara/database.js
@@ -0,0 +1,83 @@
+//insere propriedades de cada livro
+const books = [
+    {
+        name: 'Norwegian Wood by Haruki Murakami',
+        genre: 'magical realism',
+        pages: 296,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'Kafka on the Shore by Haruki Murakami',
+        genre: 'magical realism',
+        pages: 467,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'The Metamorphosis by Franz Kafka',
+        genre: 'classics',
+        pages: 201,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'Ready Player One by Ernest Cline',
+        genre: 'science fiction',
+        pages: 374,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'The Children of Húrin by J.R.R. Tolkien',
+        genre: 'fantasy',
+        pages: 313,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'Beren and Lúthien by J.R.R. Tolkien',
+        genre: 'fantasy',
+        pages: 321,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'The Shadow of the Torturer by Gene Wolfe',
+        genre: 'fantasy',
+        pages: 262,
+        recommended: true,
+        read: true,
+    },
+    {
+        name: 'The Dying Earth by Jack Vance',
+        genre: 'fantasy',
+        pages: 156,
+        recommended: true,
+        read: false,
+    },
+    {
+        name: 'The Moondark Saga by Don McQuinn',
+        genre: 'fantasy',
+        pages: 1800,
+        recommended: true,
+        read: false,
+    },
+    {
+        name: 'The Lightning Thief by Rick Riordan',
+        genre: 'fantasy',
+        pages: 377,
+        recommended: false,
+        read: false,
+    },
+    {
+        name: 'The Brothers Karamazov by Fyodor Dostoevsky',
+        genre: 'classics',
+        pages: 377,
+        recommended: true,
+        read: true,
+    },
+]
+
+//exporta o banco de dados
+module.exports = books
