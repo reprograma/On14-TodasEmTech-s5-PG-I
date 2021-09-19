cd1b08f8f6537c8f474bd2bde638d4cd41516084
Mônica Cavalcante <monica@labsit.io>
2021-09-19 16:19:34 -0300

    enviando database do projeto

--- projeto/database.js
+++ projeto/database.js
@@ -1,30 +1,63 @@
-//cria o array de objetos estudantes 
+//cria o array de objetos Livros 
 const livros = [
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
+  {
+    nome: 'O Pequeno Príncipe',
+    categoria: 'Literatura',
+    páginas: 160,
+    recomenda: 'sim',
+    leu: 'sim'
+  },
+  {
+    nome: 'O Espirito da Filosofia Medieval',
+    categoria: 'Filosofia',
+    páginas: 591,
+    recomenda: 'sim',
+    leu: 'sim'
+
+  },
+  {
+    nome: 'Negritude: usos e sentidos',
+    categoria: 'História',
+    páginas: 96,
+    recomenda: 'sim',
+    leu: 'sim'
+  },
+  {
+    nome: 'Olhos Dágua',
+    categoria: 'Literatura',
+    páginas: 116,
+    recomenda: 'sim',
+    leu: 'não'
+  },
+  {
+    nome: 'Pro Git',
+    categoria: 'Tecnologia',
+    páginas: 5994,
+    recomenda: 'sim',
+    leu: 'não'
+  },
+  {
+    nome: 'Pequeno manual antirracista',
+    categoria: 'Educação',
+    páginas: 136,
+    recomenda: 'sim',
+    leu: 'sim'
+  },
+  {
+    nome: 'Put some farofa',
+    categoria: 'Literatura',
+    páginas: 208,
+    recomenda: 'sim',
+    leu: 'sim'
+  },
+  {
+    nome: 'Orientalismo: O Oriente como invenção do Ocidente',
+    categoria: 'História',
+    páginas: 528,
+    recomenda: 'sim',
+    leu: 'sim'
+  }
 ]
 
-//exporta o array de objetos estudantes
+//exporta o array de objetos livros
 module.exports = livros
