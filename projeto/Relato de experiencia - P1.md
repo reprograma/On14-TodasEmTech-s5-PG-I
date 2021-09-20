177f13bf79c12e97479736f65bf96f568aece548
Talita Rosa Mistica <talita.rmsoareso@gmail.com>
2021-09-20 01:36:05 -0300

    adiciona Projeto I e Relato de experiência

---
+++ projeto/Relato de experiencia - P1.md
@@ -0,0 +1,19 @@
+## Relato sobre o desenvolvimento do Projeto
+
+A elaboração do primeiro projeto guiado foi bastante desafiador. Ao longo da semana busquei rever os consteúdos ministrados no sábado pela professora Tássila Bomfim, que com todo seu carisma e didática revisou conteúdos importantes e nos ensinou os passos básicos para a resolução deste desafio.
+A principio fui refazendo o passo a passo trabalhado pela professora, e aplicando os requisitos exigidos na atividade, etapa que consumiu muito do meu tempo em  virtude de contratempos que tive ao longo da semana. 
+Após desenvolver uma estrutura primária, fui, com o apoio gigante da minha esposa que atua em TI, modificando o código, pesquisando e implementando melhorias a fim de deixar o projeto num formato mais limpo e eficiente atendendo os requisitos de maneira mais efetiva. 
+Ao longo desse processo enfrentei alguns desesperos (kkkk) e dificuldades tanto pra conectar os comandos aprendidos aos requisitos pedidos, como com relação ao equipamento pois, tive problemas com meu computador, isso acabou prejudicando a entrega da atividade no tempo estipulado, tive que mudar de máquina e acabei esquecendo de configurar o git e alguns commits acabaram ficando com o nome dela, depois percebi e ajustei a configuração local. 
+Então, resumindo de forma mais objetiva posso listar as dificuldades e aprendizados que tive ao longo da semana: 
+
+### **Dificuldades**:
+ - Tive uma semana um pouco conturbada o que acabou atrasando a conclusão da atividade;
+ - Computador que não ajudou;
+ - Necessidade de mudar de máquina e configuração do git que acabou ficando errada;
+ - Dificuldade a principio de organizar e relacionar todos os conteúdos passados;
+ 
+ ### **Aprendizagens**:
+ - Não preciso ter medo de errar - faz parte do processo;
+ - Quando pedimos ajuda, os obstáculos se tornam contornáveis (não posso esquecer disso);
+ - Pedindo ajuda, pesquisando, tendo calma e paciência é possível melhorar o código e se arriscar a fazer melhorias e usar recursos diferentes mesmo que ainda não tenha visto em aula ainda <3;
+ - Consegui praticar muitas das coisas que aprendi até aqui;
--- projeto/index.js
+++ projeto/index.js
@@ -3,31 +3,68 @@ const livros = require('./database.js')
 console.log('--------------------------------------')
 console.log('     Projeto - Talita Rosa =)            ')
 console.log('--------------------------------------')
+console.log('Bem vindo ao catalogo de Livros de Rose Mistic (=')
+
+menu()
 
 // Função para obter os dados atualizados do database
 function obterDados() {
     const livros = require('./database.js')
+    return livros
 }
 
 function menu() {
-    console.log('Bem vindo ao catalogo de Livros de Talita Mistica (=')
+    
     console.log('')
     console.log('Escolha alguma das opções abaixo, informe apenas o número da opção:')
     console.log('')
-    console.log('1. Listar os books cadastrados')
+    console.log('1. Listar os livros cadastrados')
     console.log('2. Ordenar pelo número de páginas')
     console.log('3. Buscar por categoria')
-    console.log('4. books recomendados')
+    console.log('4. livros recomendados')
     console.log('5. Lista de desejos')
     console.log('6. Encerrar')
-    console.log('Obrigada por sua visita <3')
+    
+    
+    switch (obterEntrada()) {
+        case'1':
+            listarLivros()
+            menu()
+            break
+        case '2':
+            ordenarPorPagina()
+            menu()
+            break
+        case '3':
+            buscarPorCategoria()
+            menu()
+            break
+        case '4':
+            listaRecomendados()
+            menu()
+            break
+        case '5':
+            naoLidos()
+            menu()
+            break
+        case '6':    
+            console.log('Obrigada por sua visita <3')
+            break
+        default:
+            console.log('Por favor, insira uma opção válida')
+            menu()
+            break;
+        }
+}    
 
+function listarLivros(){
+    let livros = obterDados()
+    console.table(livros)
 }
-
 
 function obterEntrada() {
     const pegarEntrada = require('readline-sync')
-    const entrada = pegarEntrada.question('Digite um opcao: ').toLocaleUpperCase()
+    const entrada = pegarEntrada.question('Digite o numero da opcao desejada: ').toLocaleUpperCase()
     
     console.log('Você escolheu a opção: ', entrada)
     return entrada
@@ -40,17 +77,34 @@ function ordenarPorPagina(){
     console.table(livros)
 }
 
-function buscarCategoria() {
-    let categoria = obterDados().filter( livros => livros.categoria === qualCategoria ) //TODO RECEBER ENTRADA CATEGORIA
+function buscarPorCategoria() {
+    let categorias = obterCategorias()
+    console.table(categorias)
+    let entrada = obterEntrada()
+    
+    let listaCategoria = obterDados().filter( livros => livros.categoria === categorias[entrada] ) //TODO RECEBER ENTRADA CATEGORIA
     console.log('Esses são nossos títulos dessa categoria')
-    console.table(categoria)   
+    console.table(listaCategoria)
+}
+
+function obterCategorias() {
+    let dados = obterDados()
+    let listagem = []
+    for (let indice in dados) {
+        let categoria = dados[indice].categoria
+
+        if(!listagem.includes(categoria)){
+            listagem.push(categoria)
+        }
+    }
+    
+    return listagem
 }
 
 function listaRecomendados() {
     let livrosRecomendados = obterDados().filter(livros => livros.recomenda === 'sim')
     console.table(livrosRecomendados) 
 }
-
 
 function naoLidos() {
     const livrosDesejados = obterDados().filter(livros => livros.leu ==='não')
